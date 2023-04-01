2c06713643a11ec5f44d38c82c9a9a7bd8e6a172
Christian A <christalvarado11@gmail.com>
2023-04-01 03:37:15 -0500

    Seccion 6

--- src/app/pages/portafolio/portafolio.component.ts
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-portafolio',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class PortafolioComponent {
 
+  constructor( public productosService: ProductosService) {}
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,19 +9,34 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) { 
 
-    //console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
 
+  }
+
+
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina)=> {
 
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
-
         });
+  }
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-e856b-default-rtdb.firebaseio.com/equipo.json')
+        .subscribe( (resp: any)=> {
+
+          this.equipo = resp;
+          //console.log(resp);
+        });
+
   }
 }
